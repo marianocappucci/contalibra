13529c534f2a4654a5890a7e1a06c0b2469335d1
marianocappucci <marianocappucci@gmail.com>
2025-11-22 16:36:57 -0300

    Revert "Add fixed footer with active company info"
    This reverts commit cd2e64715134352c4394e1aa2eb7d3c07235c643.

--- vistas/layout/footer.php
+++ vistas/layout/footer.php
@@ -1,29 +1,4 @@
-<?php
-  $baseActiva = $_SESSION['db_name'] ?? ($_SESSION['user']['base_datos'] ?? null);
-  $empresaActiva = $baseActiva;
-
-  try {
-    if (class_exists('Configuracion')) {
-      $configuracionModel = new Configuracion();
-      $configuracion = $configuracionModel->get();
-      if (!empty($configuracion['nombre_fantasia'])) {
-        $empresaActiva = $configuracion['nombre_fantasia'];
-      }
-    }
-  } catch (Exception $e) {
-    // Si la consulta falla, se mantiene el valor por defecto
-  }
-
-  $empresaActiva = $empresaActiva ?: 'No configurada';
-?>
-
-<footer class="app-footer">
-  <div class="d-flex justify-content-end align-items-center">
-    <span class="small">Empresa activa: <?php echo htmlspecialchars($empresaActiva); ?></span>
-  </div>
-</footer>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="public/js/app.js"></script>
+    <script src="public/js/app.js"></script>
   </body>
 </html>
