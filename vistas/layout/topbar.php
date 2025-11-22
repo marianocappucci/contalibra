7b1b3dff7ad9938912faa62ce591201a2a01ef0d
marianocappucci <marianocappucci@gmail.com>
2025-11-22 18:14:55 -0300

    Mover menú de usuario a la barra superior

--- vistas/layout/topbar.php
+++ vistas/layout/topbar.php
@@ -22,11 +22,26 @@
     ?>
 
     <div class="ms-auto d-flex align-items-center gap-3">
-       
-
-        <a class="btn btn-outline-light btn-sm" href="index.php?controller=Auth&action=logout">
-            <i class="bi bi-box-arrow-right me-2"></i> Cerrar sesión
-        </a>
+        <div class="dropdown">
+            <a class="btn btn-outline-light btn-sm dropdown-toggle d-flex align-items-center gap-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <span class="rounded-circle bg-secondary text-white d-inline-flex align-items-center justify-content-center" style="width: 32px; height: 32px;">
+                    <i class="bi bi-person-fill"></i>
+                </span>
+                <div class="text-start">
+                    <div class="fw-semibold small"><?php echo htmlspecialchars($_SESSION['user']['nombre']); ?></div>
+                    <div class="text-white-50 small"><?php echo htmlspecialchars($_SESSION['user']['rol_nombre']); ?></div>
+                </div>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-dark shadow">
+                <li class="dropdown-header text-secondary small">Sesión</li>
+                <li>
+                    <a class="dropdown-item" href="index.php?controller=Auth&action=logout">
+                        <i class="bi bi-box-arrow-right me-2"></i>
+                        Cerrar sesión
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 
 </nav>
